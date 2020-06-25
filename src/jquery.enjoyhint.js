8bbdefb712adf3428fd81dc30adad1fd48055bd9
Claudio Maradonna <penguyman@stronzi.org>
2020-06-25 17:30:06 +0200

    Fix for invisible label

--- src/jquery.enjoyhint.js
+++ src/jquery.enjoyhint.js
@@ -337,17 +337,19 @@ CanvasRenderingContext2D.prototype.roundRect = function(x, y, w, h, r) {
                     var x = data.x || 0;
                     var y = data.y || 0;
 
-                    var tween = new Kinetic.Tween({
-                        node: that.shape,
-                        duration: 0.2,
-                        center_x: x,
-                        center_y: y,
-                        width: r * 2,
-                        height: r * 2,
-                        radius: r,
-                    });
+                    if (data.hidden == false) {
+                        var tween = new Kinetic.Tween({
+                            node: that.shape,
+                            duration: 0.2,
+                            center_x: x,
+                            center_y: y,
+                            width: r * 2,
+                            height: r * 2,
+                            radius: r,
+                        });
 
-                    tween.play();
+                        tween.play();
+                    }
 
                     var left = x - r;
                     var right = x + r;
@@ -391,17 +393,19 @@ CanvasRenderingContext2D.prototype.roundRect = function(x, y, w, h, r) {
                     var h = data.h || 0;
                     var margin = 20;
 
-                    var tween = new Kinetic.Tween({
-                        node: that.shape,
-                        duration: timeout,
-                        center_x: x,
-                        center_y: y,
-                        width: w,
-                        height: h,
-                        radius: r,
-                    });
+                    if (data.hidden == false) {
+                        var tween = new Kinetic.Tween({
+                            node: that.shape,
+                            duration: timeout,
+                            center_x: x,
+                            center_y: y,
+                            width: w,
+                            height: h,
+                            radius: r,
+                        });
 
-                    tween.play();
+                        tween.play();
+                    }
 
                     var half_w = Math.round(w / 2);
                     var half_h = Math.round(h / 2);
@@ -704,13 +708,12 @@ CanvasRenderingContext2D.prototype.roundRect = function(x, y, w, h, r) {
                             data.center_x = sides_pos.left + new_half_w;
                             data.center_y = sides_pos.top + new_half_h;
 
-                            if (that.stepData.hidden == false) {
-                                shape_data = that.renderCircle({
-                                    x: data.center_x,
-                                    y: data.center_y,
-                                    r: data.radius,
-                                });
-                            }
+                            shape_data = that.renderCircle({
+                                x: data.center_x,
+                                y: data.center_y,
+                                r: data.radius,
+                                hidden: that.stepData.hidden
+                            });
 
                             break;
 
@@ -735,18 +738,17 @@ CanvasRenderingContext2D.prototype.roundRect = function(x, y, w, h, r) {
                             data.center_x = sides_pos.left + half_w;
                             data.center_y = sides_pos.top + half_h;
 
-                            if (that.stepData.hidden == false) {
-                                shape_data = that.renderRect(
-                                    {
-                                        x: data.center_x,
-                                        y: data.center_y,
-                                        w: data.width,
-                                        h: data.height,
-                                        r: data.radius,
-                                    },
-                                    0.2
-                                );
-                            }
+                            shape_data = that.renderRect(
+                                {
+                                    x: data.center_x,
+                                    y: data.center_y,
+                                    w: data.width,
+                                    h: data.height,
+                                    r: data.radius,
+                                    hidden: that.stepData.hidden
+                                },
+                                0.2
+                            );
 
                             break;
                     }
